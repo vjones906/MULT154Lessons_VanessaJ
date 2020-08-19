6180e39d8b373b2e1d338c84cecc8cfcfc3ecc9a
Vanessa <vanessamaikojones@gmail.com>
2020-08-19 11:09:01 -0800

    Wednesday 8/19

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horMove = Input.GetAxis("Horizontal");
+
+        rbPlayer.AddForce(new Vector3(horMove, 0, 0), ForceMode.Impulse);
+    }
+}
