8418e256c2d254fcffebc022fbbaeb7187f45325
Vanessa <vanessamaikojones@gmail.com>
2020-12-06 20:03:25 -0800

    unit 7

---
+++ Assets/Scripts/WaterShaderSample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterShaderSample : MonoBehaviour
+{
+    private Renderer rend;
+    public Color setColor = Color.yellow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.SetColor("WHColor", setColor);
+    }
+}
